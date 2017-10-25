da9c460a45ef604da2d579c4d0fdf398caa71b11
DanielGreene101 <d.b.greene101@gmail.com>
2017-10-25 14:32:56 -0500

    seeded data to empty sets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,18 @@
+Computer.create!([
+  {employee_id: 4, decom_date: "2015-01-01 00:00:00", purchase_date: "2012-12-12 00:00:00", dept_id: 4},
+  {employee_id: 3, decom_date: "2015-05-01 00:00:00", purchase_date: "2012-12-05 00:00:00", dept_id: 3}
+])
 Customer.create!([
-  {first_name: "Daniel", last_name: "Greene", active_status: false},
-  {first_name: "Austin", last_name: "Kurtis", active_status: false},
-  {first_name: "Teresa", last_name: "V.", active_status: nil},
-  {first_name: "Matt", last_name: "Minner", active_status: nil},
-  {first_name: "Daniel", last_name: "Greene", active_status: nil},
-  {first_name: "Austin", last_name: "Kurtis", active_status: nil},
-  {first_name: "Teresa", last_name: "V.", active_status: nil},
-  {first_name: "Matt", last_name: "Minner", active_status: nil},
   {first_name: "Daniel", last_name: "Greene", active_status: true},
   {first_name: "Austin", last_name: "Kurtis", active_status: true},
   {first_name: "Teresa", last_name: "V.", active_status: false},
   {first_name: "Matt", last_name: "Minner", active_status: true}
 ])
-Employee.create!([
-  {department_id: 1, first_name: "Bob", last_name: "Man"},
-  {department_id: 2, first_name: "Heather", last_name: "Girl"}
+Department.create!([
+  {expense_budget: 30000.0, employee_id: 3, department_name: "customer survice"},
+  {expense_budget: 456763.0, employee_id: 2, department_name: "Tech"}
 ])
 Order.create!([
-  {payment_id: 1, customer_id: 1, price: 4.99},
-  {payment_id: 2, customer_id: 2, price: 33.99},
-  {payment_id: 3, customer_id: 3, price: 33.67},
-  {payment_id: 4, customer_id: 4, price: 66.88},
-  {payment_id: 1, customer_id: 1, price: 4.99},
-  {payment_id: 2, customer_id: 2, price: 33.99},
-  {payment_id: 3, customer_id: 3, price: 33.67},
-  {payment_id: 4, customer_id: 4, price: 66.88},
   {payment_id: 1, customer_id: 1, price: 4.99},
   {payment_id: 2, customer_id: 2, price: 33.99},
   {payment_id: 3, customer_id: 3, price: 33.67},
@@ -39,31 +27,9 @@ OrderProduct.create!([
 PaymentType.create!([
   {type_name: "MASTERCARD"},
   {type_name: "VISA"},
-  {type_name: "ACH"},
-  {type_name: "MASTERCARD"},
-  {type_name: "VISA"},
-  {type_name: "ACH"},
-  {type_name: "MASTERCARD"},
-  {type_name: "VISA"},
   {type_name: "ACH"}
 ])
 Product.create!([
-  {type_id: 4, customer_id: 4, price: 300.0, title: "Spacely Sprockets", description: "Space stuff"},
-  {type_id: 5, customer_id: 3, price: 30.0, title: "Hat", description: "Greatest hat of all time"},
-  {type_id: 2, customer_id: 1, price: 3000000.0, title: "Stary Night", description: "Don't cutt off ears man"},
-  {type_id: 3, customer_id: 2, price: 4000000000.0, title: "Matt's Dignity", description: "He needs MONEY"},
-  {type_id: 4, customer_id: 2, price: 40000.0, title: "Car", description: "VROOM VROOM ZOOM"},
-  {type_id: 5, customer_id: 1, price: 400.0, title: "Propeller Hat", description: "Brrrrrrr"},
-  {type_id: 2, customer_id: 3, price: 6.0e+17, title: "Daniel", description: "He'll do it"},
-  {type_id: 3, customer_id: 4, price: 14.0, title: "Head (and other) shaving cream", description: "KEEP YA SMOOTH LIKE A BABY'S BOTTOM"},
-  {type_id: 4, customer_id: 4, price: 300.0, title: "Spacely Sprockets", description: "Space stuff"},
-  {type_id: 5, customer_id: 3, price: 30.0, title: "Hat", description: "Greatest hat of all time"},
-  {type_id: 2, customer_id: 1, price: 3000000.0, title: "Stary Night", description: "Don't cutt off ears man"},
-  {type_id: 3, customer_id: 2, price: 4000000000.0, title: "Matt's Dignity", description: "He needs MONEY"},
-  {type_id: 4, customer_id: 2, price: 40000.0, title: "Car", description: "VROOM VROOM ZOOM"},
-  {type_id: 5, customer_id: 1, price: 400.0, title: "Propeller Hat", description: "Brrrrrrr"},
-  {type_id: 2, customer_id: 3, price: 6.0e+17, title: "Daniel", description: "He'll do it"},
-  {type_id: 3, customer_id: 4, price: 14.0, title: "Head (and other) shaving cream", description: "KEEP YA SMOOTH LIKE A BABY'S BOTTOM"},
   {type_id: 4, customer_id: 4, price: 300.0, title: "Spacely Sprockets", description: "Space stuff"},
   {type_id: 5, customer_id: 3, price: 30.0, title: "Hat", description: "Greatest hat of all time"},
   {type_id: 2, customer_id: 1, price: 3000000.0, title: "Stary Night", description: "Don't cutt off ears man"},
@@ -77,18 +43,8 @@ ProductType.create!([
   {prod_type: "Artwork", description: "Wow such art. Many paints"},
   {prod_type: "Books", description: "knowledge and pages"},
   {prod_type: "Vintage", description: "OLD AND STUFF"},
-  {prod_type: "Clothing", description: "prevents you from being nude fairly well"},
-  {prod_type: "Artwork", description: "Wow such art. Many paints"},
-  {prod_type: "Books", description: "knowledge and pages"},
-  {prod_type: "Vintage", description: "OLD AND STUFF"},
-  {prod_type: "Clothing", description: "prevents you from being nude fairly well"},
-  {prod_type: "Artwork", description: "Wow such art. Many paints"},
-  {prod_type: "Books", description: "knowledge and pages"},
-  {prod_type: "Vintage", description: "OLD AND STUFF"},
   {prod_type: "Clothing", description: "prevents you from being nude fairly well"}
 ])
 TrainingProgram.create!([
-  {start_date: "2015-12-31 23:59:59", end_date: "2016-12-31 23:59:59", max_students: 33, prog_name: "test!!!!! no delete ever"},
-  {start_date: "2015-12-31 23:59:59", end_date: "2016-12-31 23:59:59", max_students: 33, prog_name: "test!!!!! no delete ever"},
   {start_date: "2015-12-31 23:59:59", end_date: "2016-12-31 23:59:59", max_students: 33, prog_name: "test!!!!! no delete ever"}
 ])
